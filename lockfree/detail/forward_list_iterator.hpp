b1d833ce1aa933f07740de206086e6b2ad22b66a
Jorge Bellon <jorge.bellon@bsc.es>
2016-06-08 18:26:01 +0200

    Fixing errors in iterators and nodes
    Changing from class to struct definition.
    Fixed wrong compile guards.
    Fixed wrong member type definition.
    Included additional member functions.

---
+++ lockfree/detail/forward_list_iterator.hpp
@@ -0,0 +1,164 @@
+
+#ifndef FORWARD_ALLOCATOR_H
+#define FORWARD_ALLOCATOR_H
+
+#include "forward_list_node.hpp"
+
+#include <iterator>
+#include <memory>
+
+namespace ads {
+namespace detail {
+
+template< typename T >
+struct forward_list_iterator : public std::iterator< std::forward_iterator_tag, T >
+{
+// Member types
+		typedef forward_list_iterator<T> self_type;
+		typedef forward_list_node_base   node_base_type;
+		typedef forward_list_node<T>     node_type;
+		typedef node_base_type*          node_pointer;
+
+		typedef T&                       reference;
+		typedef T*                       pointer;
+
+// Member attributes
+		node_pointer _node;
+
+// Member functions
+		forward_list_iterator() :
+			_node()
+		{
+		}
+
+		explicit forward_list_iterator( node_pointer position ) :
+			_node( position )
+		{
+		}
+
+		bool operator==( const self_type& other ) const noexcept
+		{
+			return _node == other._node;
+		}
+
+		bool operator!=( const self_type& other ) const noexcept
+		{
+			return _node != other._node;
+		}
+
+		reference operator*() const noexcept
+		{
+			return *static_cast<node_type*>(_node)->valuePtr();
+		}
+
+		pointer operator->() const noexcept
+		{
+			return static_cast<node_type*>(_node)->valuePtr();
+		}
+
+		// ++it
+		self_type& operator++() noexcept
+		{
+			_node = _node->next();
+			return *this;
+		}
+
+		// it++
+		self_type operator++(int) noexcept
+		{
+			self_type tmp = *this;
+			_node = _node->next();
+			return tmp;
+		}
+
+		self_type next() const noexcept
+		{
+			if( _node )
+				return forward_list_iterator( _node->next() );
+			else
+				return forward_list_iterator( nullptr );
+		}
+};
+
+template< typename T >
+struct forward_list_const_iterator : public std::iterator< std::forward_iterator_tag, T >
+{
+// Member types
+		typedef forward_list_const_iterator<T> self_type;
+		typedef forward_list_iterator<T>       iterator;
+		typedef forward_list_node_base         node_base_type;
+		typedef forward_list_node<T>           node_type;
+		typedef node_base_type*                node_pointer;
+
+		typedef const T&                             const_reference;
+		typedef const T*                             const_pointer;
+
+// Member attributes
+		node_pointer _node;
+
+// Member functions
+		forward_list_const_iterator() :
+			_node()
+		{
+		}
+
+		explicit forward_list_const_iterator( node_pointer position ) :
+			_node( position )
+		{
+		}
+
+		// Implicit conversion
+		forward_list_const_iterator( const iterator& other ) :
+			_node( other._node )
+		{
+		}
+
+		bool operator==( const self_type& other ) const noexcept
+		{
+			return _node == other._node;
+		}
+
+		bool operator!=( const self_type& other ) const noexcept
+		{
+			return _node != other._node;
+		}
+
+		const_reference operator*() const noexcept
+		{
+			return *static_cast<node_type*>(_node)->valuePtr();
+		}
+
+		const_pointer operator->() const noexcept
+		{
+			return static_cast<node_type*>(_node)->valuePtr();
+		}
+
+		// ++it
+		self_type& operator++() noexcept
+		{
+			_node = _node->next();
+			return *this;
+		}
+
+		// it++
+		self_type operator++(int) noexcept
+		{
+			self_type tmp = *this;
+			_node = _node->next();
+			return tmp;
+		}
+
+		self_type next() const noexcept
+		{
+			if( _node )
+				return forward_list_const_iterator( _node->next() );
+			else
+				return forward_list_const_iterator( nullptr );
+		}
+};
+
+} // namespace detail
+} // namespace ads
+
+#endif // FORWARD_ALLOCATOR_H
+
--- lockfree/detail/forward_list_node.hpp
+++ lockfree/detail/forward_list_node.hpp
@@ -1,50 +1,82 @@
 
-#ifdef FORWARD_LIST_NODE_H
+#ifndef FORWARD_LIST_NODE_H
 #define FORWARD_LIST_NODE_H
+
+#include <utility>
 
 namespace ads {
 namespace detail {
 
+struct forward_list_node_base {
+
+// Member functions
+	forward_list_node_base() :
+		_next()
+	{
+	}
+
+	forward_list_node_base( forward_list_node_base* next ) :
+		_next( next )
+	{
+	}
+
+	forward_list_node_base* next() const noexcept
+	{
+		return _next;
+	}
+
+// Data members
+	forward_list_node_base* _next;
+};
+
 /**
  * @brief A base class for %forward_list.
  */
-template<
-	class T,
-	class Allocator
-	>
-class forward_list_base<T,Allocator>::node {
-	public:
+template< typename T >
+struct forward_list_node : public forward_list_node_base {
+
 // Member types
-		typedef T value_type;
-		typedef value_type& reference;
-		typedef const value_type& const_reference;
-		typedef Allocator value_allocator_type;
+	typedef T&       reference;
+	typedef const T& const_reference;
+	typedef T*       pointer;
+	typedef const T* const_pointer;
 
 // Member functions
-		node() = delete;
+	forward_list_node() = delete;
 
-		node( const node& ) = delete;
+	forward_list_node( const forward_list_node& ) = delete;
 
-		node( Allocator allocator = Allocator(), const_reference value ) :
-			_next( nullptr ),
-			_value( value )
-		{
-		}
+	forward_list_node( forward_list_node&& other ) :
+		forward_list_node_base( std::forward( other ) ),
+		_value( std::move( other._value ) )
+	{
+	}
 
-		template < typename... Args >
-		node( Allocator allocator = Allocator(), Args&&... args ) :
-			_next( nullptr ),
-			_value( std::forward<Args>(args)... )
-		{
-		}
+	forward_list_node( const_reference value ) :
+		forward_list_node_base(),
+		_value( value )
+	{
+	}
 
-		virtual ~node()
-		{
-		}
+	template < typename... Args >
+	forward_list_node( Args&&... args ) :
+		forward_list_node_base(),
+		_value( std::forward<Args>(args)... )
+	{
+	}
 
-	private:
-		node*     _next;
-		T         _value;
+	pointer valuePtr()
+	{
+		return &_value;
+	}
+
+	const_pointer valuePtr() const
+	{
+		return &_value;
+	}
+
+// Member elements
+	T     _value;
 };
 
 } // namespace detail
