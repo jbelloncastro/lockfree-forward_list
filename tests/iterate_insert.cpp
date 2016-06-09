60fcba5fce377ca05a78037801903783c780af93
Jorge Bellon <jorge.bellon@bsc.es>
2016-06-09 15:47:45 +0200

    Extend tests for several construction routines

--- tests/iterate_insert.cpp
+++ tests/iterate_insert.cpp
@@ -1,15 +1,49 @@
 
 #include <forward_list.hpp>
+#include <forward_list>
 
 #include <gtest/gtest.h>
 
-TEST(IteratorsTest, Values) {
-	ads::forward_list<int> list{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+TEST(ConstructorTest, Default) {
+	std::forward_list<int> list;
+	ASSERT_EQ( list.empty(), true );
+}
+
+TEST(ConstructorTest, InitializerList) {
+	std::forward_list<int> list{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 	int i = 1;
 	for( int& v: list ) {
-		EXPECT_EQ( i++, v );
+		ASSERT_EQ( i++, v );
 	}
-   //ASSERT_EQ( value, function );
+	//EXPECT_EQ( value, function );
+}
+
+TEST(ConstructorTest, ValueInitializer) {
+	int amount = 5;
+	int value = 4;
+	ads::forward_list<int> list( amount, value );
+
+        int count = 0;
+	for( int& v: list ) {
+		ASSERT_EQ( v, value );
+		count++;
+	}
+	ASSERT_EQ( count, amount );
+}
+
+TEST(ConstructorTest, RangeInitializer ) {
+	int array[5] = {0, 1, 2, 3, 4};
+	auto begin = std::begin(array);
+	auto end = std::end(array);
+	ads::forward_list<int> list( ++begin, --end );
+
+	int amount = std::distance( begin, end );
+	int count = 0;
+	for( int& v: list ) {
+		ASSERT_EQ( v, *begin++ );
+		count++;
+	}
+	ASSERT_EQ( count, amount );
 }
 
 int main(int argc, char **argv) {
